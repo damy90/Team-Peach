d95304f18422483fef3ec4df49e6f5db4f69730f
Kaloyan Marshalov <kaloyan.marshalov@gmail.com>
2015-03-22 11:06:19 +0200

    Added two properties to Food class. Added Methods for increasing the FoodValue of Meat/Plant.

--- Tamagochi/Models/Models/Foods/Food.cs
+++ Tamagochi/Models/Models/Foods/Food.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Models
+﻿namespace Models
 {
     public abstract class Food
     {
+        public Food()
+        {
+        }
+        public Food(int foodValue, int feedInterval)
+        {
+            this.FoodValue = foodValue;
+            this.FeedInterval = feedInterval;
+        }
+        //The increase in Feed
+        public int FoodValue { get; set; }
+        //The time until the same food can be given to the animaml in seconds
+        public int FeedInterval { get; set; }
     }
 }
--- Tamagochi/Models/Models/Foods/Plant.cs
+++ Tamagochi/Models/Models/Foods/Plant.cs
@@ -1,11 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Models
+﻿namespace Models
 {
     public abstract class Plant : Food
     {
+        //How much the the plant can grow (the biggest increase possible in foodValue)
+        public int GrowthPotential { get; set; }
+        //Logic for increasing the Plant's food value
+        public void GrowBigger(int growthTime)
+        {
+            var foodValue = base.FoodValue;
+            base.FeedInterval += growthTime; //Cannot give the same plant to the animal until the plant is done growing.
+
+            /*
+             *If the growth time is bigger than the plant's potential, remove points from the food value (the plant rottens).
+             *Else add point to foodValue, equal to growth time.
+            */
+            if (growthTime > this.GrowthPotential)
+            {
+                base.FoodValue -= growthTime - this.GrowthPotential;
+                if (base.FoodValue < 0)
+                {
+                    base.FoodValue = 0;
+                }
+            }
+            else
+            {
+                base.FoodValue += growthTime;
+            }
+        }
     }
 }
