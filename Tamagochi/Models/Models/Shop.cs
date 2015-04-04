94cc884acbc4dd88df86683d733e21d8d76ecd3f
kament <kamen_t@abv.bg>
2015-04-04 18:26:08 +0300

    added shop
    Conf

--- Tamagochi/Models/Models/Shop.cs
+++ Tamagochi/Models/Models/Shop.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Models
+﻿namespace Models
 {
+    using System;
+    using System.Collections.Generic;
+
     public class Shop
     {
         private static Shop _instance;
@@ -24,13 +22,41 @@ namespace Models
             return _instance;
         }
 
-        //public void BuyItem(IBuyable item, Player buyer)
-        //{
-        //    if (item.Price <= buyer.Coins)
-        //    {
-        //        buyer.Coins -= item.Price;
-        //        buyer.AddItem(item);
-        //    }
-        //}
+        public List<string> FoodsInStore
+        {
+            get
+            {
+                return new List<string> { "Banana", "Pear", "FishMeat", "Pizza", "Steak" };
+            }
+        }
+
+        private IBuyable GetFood(string foodName)
+        {
+            switch (foodName.ToLower())
+            {
+                case "banana":
+                    return new Banana();
+                case "fishmeat":
+                    return new FishMeat();
+                case "pear":
+                    return new Pear();
+                case "pizza":
+                    return new Pizza();
+                case "steak":
+                    return new Steak();
+                default:
+                    throw new ArgumentException("This food does not exist!");
+            }
+        }
+
+        public void BuyItem(string item, Player buyer)
+        {
+            var food = this.GetFood(item);
+            if (food.Price <= buyer.Coins)
+            {
+                buyer.Coins -= food.Price;
+                buyer.AddItem(food);
+            }
+        }
     }
 }
