ef490d83cf7ee7e7d6d965c3ff54250974d9a69d
dtopalov <dimiter.topalov79@gmail.com>
2015-04-04 16:07:56 +0300

    Player modified

--- Tamagochi/Models/Models/Player.cs
+++ Tamagochi/Models/Models/Player.cs
@@ -5,53 +5,26 @@ using System.Xml.Serialization;
 
 namespace Models
 {
-    //singleton (only one player can be initialized)
-    public sealed class Player
+    public class Player
     {
-        const int StartCoins = 500;
+        const int StartCoins = 100;
 
-        private string name;
+        private int points;
         private int coins;
         private Animal pet;
-        private List<IBuyable> food;
-        private static Player _instance;
-
-        private Player()
-        {
-
-        }
-
-        private Player(string name, Animal animal)
+        
+        public Player(Animal animal)
         {
             this.coins = StartCoins;
-            this.name = name;
-            this.pet = animal;
-            this.food = new List<IBuyable>();
+            this.Pet = animal;
+            this.Points = 0;
         }
 
-        public static Player Initialize(string name, Animal animal)
-        {
-            if (_instance == null)
-            {
-                return _instance = new Player(name, animal);
-            }
-
-            throw new InvalidOperationException("A player already exists!");
+        public int Points {
+            get { return this.points; }
+            set { this.points = value; }
         }
-
-        public static Player GetInstance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    throw new ArgumentNullException("No player has been initialized!");
-                }
-
-                return _instance;
-            }
-        }
-
+        
         public int Coins
         {
             get
@@ -70,41 +43,17 @@ namespace Models
             {
                 return this.pet;
             }
+            private set { this.pet = value; }
         }
 
-        public List<IBuyable> Food
-        {
-            get
-            {
-                return new List<IBuyable>(this.food);
-            }
-            private set
-            {
-                ;
-            }
-        }
-
-        public void AddItem(IBuyable item)
-        {
-            this.Food.Add(item);
-        }
-
-        //cannot be tested until a game can be started
-        public void Serialize(string path = "../../")
-        {
-            XmlSerializer xml = new XmlSerializer(this.GetType());
-            StreamWriter file = new StreamWriter(path + this.name + ".xml");
-            xml.Serialize(file, this);
-            file.Close();
-        }
-
-        public Player Deserialize(string path = "../../player.xml")
-        {
-            XmlSerializer xml = new XmlSerializer(this.GetType());
-            StreamReader file = new StreamReader(path);
-            _instance = (Player)xml.Deserialize(file);
-            file.Close();
-            return _instance;
-        }
+        //public Player Deserialize(string path = "../../player.xml")
+        //{
+        // TODO: Dani, make this happen
+        //    XmlSerializer xml = new XmlSerializer(this.GetType());
+        //    StreamReader file = new StreamReader(path);
+        //    _instance = (Player)xml.Deserialize(file);
+        //    file.Close();
+        //    return _instance;
+        //}
     }
 }
