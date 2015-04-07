29794bd927c88eb891fc5d2b87ef11e9b6b7de3a
VladimirDimov <vdimov1124@gmail.com>
2015-04-07 17:31:18 +0300

    Parrot

--- Tamagochi/Models/Models/Animals/Parrot.cs
+++ Tamagochi/Models/Models/Animals/Parrot.cs
@@ -9,10 +9,10 @@
     {
         #region Constants
         private const string ParrotSoundPath =      @"..\..\..\Models\Resourses\Sound\ParrotSound.wav";
-        private const string ParrotPictureDefault = @"..\..\..\Models\Resourses\Pictures\Cat\initial.png";
-        private const string ParrotPictureEating =  @"..\..\..\Models\Resourses\Pictures\Cat\eat.png";
-        private const string ParrotPictureUnahppy = @"..\..\..\Models\Resourses\Pictures\Cat\sad.png";
-        private const string ParrotPicturePlaying = @"..\..\..\Models\Resourses\Pictures\Cat\play.png";
+        private const string ParrotPictureDefault = @"..\..\..\Models\Resourses\Pictures\Parrot\initial.png";
+        private const string ParrotPictureEating =  @"..\..\..\Models\Resourses\Pictures\Parrot\eat.png";
+        private const string ParrotPictureUnahppy = @"..\..\..\Models\Resourses\Pictures\Parrot\sad.png";
+        private const string ParrotPicturePlaying = @"..\..\..\Models\Resourses\Pictures\Parrot\play.png";
         private const int HappinessIncrement = 20;
         private const int PlayPointsIncrement = 20;
         #endregion
